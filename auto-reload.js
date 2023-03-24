9da0232f616c285caaecaae70bfb97d1d372e0ae
Jim Blandy <jimb@red-bean.com>
2023-03-23 17:01:31 -0700

    Document auto-reload rules a bit better.

--- auto-reload.js
+++ auto-reload.js
@@ -1,3 +1,8 @@
+/// This is meant to be used with the [serve-live] static directory
+/// server, which notifies the page when files in the directory have
+/// changed, using server-sent events.
+///
+/// [serve-live]: https://github.com/jimblandy/serve-live/
 var events = new EventSource("events");
 events.addEventListener("files-changed", (event) => {
     events.close();
@@ -8,7 +13,7 @@ events.onerror = (err) => {
     console.log("server-sent event source 'event' failed");
 };
 
-// Avoid error when page is reloaded.
+// Avoid Firefox error when page is reloaded.
 // https://bugzilla.mozilla.org/show_bug.cgi?id=833462#c11
 window.addEventListener('beforeunload', () => {
     events.close();
