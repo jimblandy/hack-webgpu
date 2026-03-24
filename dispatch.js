336f77e596ef528218897d8f47bdc62132798baf
Jim Blandy <jimb@red-bean.com>
2026-03-23 18:06:00 -0700

    Don't require hacks to be explicitly imported and enumerated.

--- dispatch.js
+++ dispatch.js
@@ -1,11 +1,6 @@
-import triangle from './triangle.js';
-import compute from './compute.js';
-
-const hacks = {
-    triangle,
-    compute,
-};
-
 const params = new URLSearchParams(window.location.search);
-const entry_point = hacks[params.get('hack') || "triangle"];
-entry_point();
+const entry_point = params.get('hack') || "triangle";
+if (!/[a-zA-Z0-9_-]+/.test(entry_point)) {
+    throw new Error(`Weird entry point name: ${entry_point}`);
+}
+import(`./${entry_point}.js`).then((main) => { main.default() });
